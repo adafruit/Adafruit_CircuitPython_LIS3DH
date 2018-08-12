2e08d96f81f36db7033b8839e20ccaf6a86766e4
Kattni Rembor <kattni@adafruit.com>
2018-08-12 16:04:06 -0400

    Updates to examples

--- examples/lis3dh_simpletest.py
+++ examples/lis3dh_simpletest.py
@@ -1,35 +1,24 @@
-# Accelerometer example.
-# Reads the accelerometer x, y, z values and prints them every tenth of a second.
-# Open the serial port after running to see the output printed.
-# Author: Tony DiCola
 import time
 import board
+import digitalio
 import busio
 import adafruit_lis3dh
 
-
-# Uncomment _one_ of the hardware setups below depending on your wiring:
-
 # Hardware I2C setup. Use the CircuitPlayground built-in accelerometer if available;
 # otherwise check I2C pins.
-# pylint: disable=no-member
 if hasattr(board, 'ACCELEROMETER_SCL'):
     i2c = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
-    lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19)
+    int1 = digitalio.DigitalInOut(board.ACCELEROMETER_INTERRUPT)
+    lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19, int1=int1)
 else:
     i2c = busio.I2C(board.SCL, board.SDA)
-    lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c)
-
-# Software I2C setup:
-#import bitbangio
-#i2c = bitbangio.I2C(board.SCL, board.SDA)
-#lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c)
+    int1 = digitalio.DigitalInOut(board.D9)  # Set this to the correct pin for the interrupt!
+    lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
 
 # Hardware SPI setup:
-#import busio
-#spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-#cs = busio.DigitalInOut(board.D6)  # Set to appropriate CS pin!
-#lis3dh = adafruit_lis3dh.LIS3DH_SPI(spi, cs)
+# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+# cs = digitalio.DigitalInOut(board.D9)  # Set to appropriate CS pin!
+# lis3dh = adafruit_lis3dh.LIS3DH_SPI(spi, cs)
 
 
 # Set range of accelerometer (can be RANGE_2_G, RANGE_4_G, RANGE_8_G or RANGE_16_G).
@@ -40,6 +29,6 @@ while True:
     # Read accelerometer values (in m / s ^ 2).  Returns a 3-tuple of x, y,
     # z axis values.  Divide them by 9.806 to convert to Gs.
     x, y, z = [value / adafruit_lis3dh.STANDARD_GRAVITY for value in lis3dh.acceleration]
-    print('x = {}G, y = {}G, z = {}G'.format(x, y, z))
+    print("x = %0.3f G, y = %0.3f G, z = %0.3f G" % (x, y, z))
     # Small delay to keep things responsive but give time for interrupt processing.
     time.sleep(0.1)
