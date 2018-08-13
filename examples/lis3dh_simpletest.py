6a487c30304ce0b7498684c5a3230e509829dba8
Kattni Rembor <kattni@adafruit.com>
2018-08-13 10:51:59 -0400

    Update pins to match guide

--- examples/lis3dh_simpletest.py
+++ examples/lis3dh_simpletest.py
@@ -12,7 +12,7 @@ if hasattr(board, 'ACCELEROMETER_SCL'):
     lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19, int1=int1)
 else:
     i2c = busio.I2C(board.SCL, board.SDA)
-    int1 = digitalio.DigitalInOut(board.D9)  # Set this to the correct pin for the interrupt!
+    int1 = digitalio.DigitalInOut(board.D6)  # Set this to the correct pin for the interrupt!
     lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
 
 # Hardware SPI setup:
